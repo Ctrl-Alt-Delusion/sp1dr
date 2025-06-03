f9bee593ce64cc861dc0e356d399931af18cbde7
Danial Jumagaliyev <djumagaliyev@myseneca.ca>
2025-06-02 22:56:48 -0400

    [task] meth

--- src/math/math.hpp
+++ src/math/math.hpp
@@ -1,8 +1,53 @@
 #pragma once
 
+#include <math.h>
+
 namespace MATH {
     template<typename T>
     struct Vec2 {
         T x, y;
+
+        T magnitude() const {
+            return sqrt(x * x + y * y);
+        }
+
+        inline Vec2 operator+(const Vec2& other) const {
+            return {x + other.x, y + other.y};
+        }
+
+        inline T operator*(const Vec2& other) const {
+            return x * other.x + y * other.y;
+        }
+
+        inline bool operator==(const Vec2& other) const {
+            return (x == other.x && y == other.y);
+        }
+    };
+
+    template<typename T>
+    struct Vec3 {
+        T x, y, z;
+
+        T magnitude() const {
+            return sqrt(x * x + y * y + z * z);
+        }
+
+        Vec3 cross(const Vec3& other) const {
+            return {y * other.z - z * other.y,
+                    z * other.x - x * other.z,
+                    x * other.y - y * other.x};
+        }
+
+        inline Vec3 operator+(const Vec3& other) const {
+            return {x + other.x, y + other.y, z + other.z};
+        }
+
+        inline T operator*(const Vec3& other) const {
+            return x * other.x + y * other.y + z * other.z;
+        }
+
+        inline bool operator==(const Vec3& other) const {
+            return (x == other.x && y == other.y && z == other.z);
+        }
     };
 }
