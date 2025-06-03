f2ab439956a8d12141696ddf137e132b660cc055
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-06-02 21:40:57 -0400

    [task] init

---
+++ src/core/core.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace CORE {
+
+    class Core {
+    public:
+    private:
+    };
+    
+} // CORE
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "We live bois!" << std::endl;
+    return 0;
+}
