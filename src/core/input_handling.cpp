398098a565b8b3014af5105f1484701e34279bb2
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-06-06 20:01:45 -0400

    [bugfix] fixed linked duplicate symbol error

--- src/core/input_handling.cpp
+++ src/core/input_handling.cpp
@@ -3,16 +3,28 @@
 namespace CORE {
     // ==== PLATFORM-SPECIFIC: Input Stuff ====
     #if defined(_WIN32)
-
     bool kbhit() {
         return _kbhit();
     }
-
+    
     char getch() {
         return _getch();
     }
     #else
-
+    void enable_raw_mode() {
+        termios term;
+        tcgetattr(STDIN_FILENO, &term);
+        term.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    }
+    
+    void disable_raw_mode() {
+        termios term;
+        tcgetattr(STDIN_FILENO, &term);
+        term.c_lflag |= (ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    }
+    
     bool kbhit() {
         timeval tv = {0L, 0L};
         fd_set fds;
@@ -20,12 +32,11 @@ namespace CORE {
         FD_SET(STDIN_FILENO, &fds);
         return select(STDIN_FILENO + 1, &fds, nullptr, nullptr, &tv) > 0;
     }
-
+    
     char getch() {
         char ch = 0;
         read(STDIN_FILENO, &ch, 1);
         return ch;
     }
-
     #endif
 }
--- src/core/input_handling.hpp
+++ src/core/input_handling.hpp
@@ -1,38 +1,23 @@
 #pragma once
-
 #include <iostream>
 #include <thread>
 #include <chrono>
-
 #if defined(_WIN32)
-    #include <conio.h>
+#include <conio.h>
 #else
-    #include <termios.h>
-    #include <unistd.h>
-    #include <fcntl.h>
-    #include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/select.h>
 #endif
 
 namespace CORE {
-    // ==== PLATFORM-SPECIFIC: Input Stuff ====
+    // Function declarations only
     #if !defined(_WIN32)
-
-    void enable_raw_mode() {
-        termios term;
-        tcgetattr(STDIN_FILENO, &term);
-        term.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    }
-
-    void disable_raw_mode() {
-        termios term;
-        tcgetattr(STDIN_FILENO, &term);
-        term.c_lflag |= (ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    }
-
+    void enable_raw_mode();
+    void disable_raw_mode();
     #endif
-
+    
     bool kbhit();
     char getch();
 }
