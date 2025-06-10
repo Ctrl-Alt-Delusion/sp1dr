8e36f7edd9458e3b575e7af3cbbc9d8222817059
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-06-10 02:05:57 -0400

    [task] color + textures

---
+++ src/color/color_manager.hpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <tuple>
+#include <sstream>
+#include <iomanip>
+
+namespace COLOR {
+
+static std::string RGB_to_HEX(int r, int g, int b) {
+    std::ostringstream oss;
+    oss << "#" << std::hex << std::setfill('0') << std::setw(2) << r
+        << std::setw(2) << g << std::setw(2) << b;
+    return oss.str();
+}
+
+static std::string RGB_to_HEX(const std::tuple<int, int, int>& rgb) {
+    return RGB_to_HEX(std::get<0>(rgb), std::get<1>(rgb), std::get<2>(rgb));
+}
+
+
+} // namespace COLOR
+
---
+++ src/texture/texture.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace TEXTURE {
+
+class Texture {
+protected:
+    std::string texture_pattern;
+
+public:
+    Texture(std::string texture_pattern);
+    virtual std::vector<std::string> generate_texture() = 0;
+    virtual ~Texture() = default;
+};
+
+class Texture2D : protected Texture {
+private:
+    size_t height;
+    size_t width;
+
+public:
+    Texture2D(size_t width, size_t height, std::string pattern);
+    void set_dimensions(size_t width, size_t height);
+    std::vector<std::string> generate_texture() override;
+};
+
+}
