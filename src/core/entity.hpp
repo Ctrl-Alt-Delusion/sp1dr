b99cd44a628a7a68875ab8f5182c3f797da337e2
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-06-15 21:39:05 -0400

    [bugfix] stupid bug
    Co-authored-by: Danqzq <djumagaliyev@myseneca.ca>

--- src/core/entity.hpp
+++ src/core/entity.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
-#include "entity_manager.hpp"
-
 #include <cstdint>
 #include <bitset>
 
 namespace CORE {
+    class EntityManager; // forward declaration
+
     constexpr uint64_t MAX_ENTITIES = 1000000000;
     constexpr uint64_t ILLEGAL_ENTITY = MAX_ENTITIES + 1;
     constexpr uint16_t MAX_COMPONENTS = 64;
-
-    class EntityManager; // Forward declaration
 
     class Entity {
     private:
@@ -18,53 +16,38 @@ namespace CORE {
         static uint32_t next_entity_id;
         static uint32_t component_type_id;
         std::bitset<MAX_COMPONENTS> component_mask;
-        EntityManager* entityMan {};
+        EntityManager* entityMan {}; // this works fine with just forward declaration
 
-        // we are friends with the kitty man ≽^•⩊•^≼
         friend class EntityManager;
 
-        // Some sort of Entity manager*
     public:
         Entity(EntityManager* em) : id(next_entity_id++), entityMan(em) {}
 
-        uint64_t get_id() const {
+        uint64_t get_id() const{
             return this->id;
         }
 
-        // Bary wishes he was this cool 
         template <typename T> 
-        bool has_component() const {
+        bool has_component() const{
             return component_mask[get_component_type<T>()];
         }
 
         template <typename T>
-        T& get_component() const {
-            return entityMan->get_component<T>(this->id);
-        }
+        T& get_component() const;
 
         template <typename T>
-        T& add_component() {
-            this->component_mask.set(get_component_type<T>());
-            return entityMan->add_component<T>(this->id);
-        }
+        T& add_component();
 
         template <typename T>
-        void remove_component() {
-            this->component_mask.reset(get_component_type<T>());
-            entityMan->remove_component<T>(this->id);
-        }
+        void remove_component();
 
-        // Generate component_map index for new component type
         template<typename T>
         static uint32_t get_component_type() {
             static uint8_t type_id = component_type_id++;
             return type_id;
         }
     };
-    
+
     uint32_t Entity::next_entity_id    = 0;
     uint32_t Entity::component_type_id = 0;
-
-
-
-} // namespace CORE
+}
